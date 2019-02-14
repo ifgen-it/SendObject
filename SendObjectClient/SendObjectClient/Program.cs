21814a0e19b8bffe85be12517b38755c05dc5d56
Evgen Smirnov <ifgen.it@gmail.com>
2019-02-14 20:59:30 +0300

    just began

---
+++ SendObjectClient/SendObjectClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SendObjectClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hello");
+
+
+
+            Console.Read();
+        }
+    }
+}
